3907fad5b0beef90ecc463793780b5c6fd031cbe
Walter Low <79463731+walterlow52@users.noreply.github.com>
2024-03-15 21:56:30 -0400

    Update playlist_table.js

--- playlist_table.js
+++ playlist_table.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+function playlist(table) {
+  return "Hello";
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<playlist />);
