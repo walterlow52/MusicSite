8d936f533820d9938dd061fdc1952e081318a849
Walter Low <79463731+walterlow52@users.noreply.github.com>
2024-03-23 23:17:41 -0400

    Delete playlist_table.js

--- playlist_table.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const container = document.getElementById('tracklist_display');
-const root = ReactDOM.createRoot(container);
-root.render(playlist_table);
