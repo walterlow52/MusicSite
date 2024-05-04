63bf862b873e1387be1693feef2d75fbd585ae66
BroncoSSB <walterlow52@gmail.com>
2024-05-03 22:11:45 -0400

    added changes to draft playlist_table

--- playlist_table.js
+++ playlist_table.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './music_styling.css';
+
+const total = 311;
 
 function playlist_table(props) {
-    return <h1> Welcome to the Playlist Table! </h1>;
+    return (
+        <>
+            <h1> Welcome to the Playlist Table! </h1> 
+            <h2> This song contains {total} songs in the playlist. </h2>
+        </>
+    );
 }
 
 const container = document.getElementById("root");
