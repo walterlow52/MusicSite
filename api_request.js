13685e719b318a7437c47334b7c1d4b50832ea7f
walterlow52 <walterlow52.com>
2025-10-25 17:16:40 -0400

    added new songs

--- api_request.js
+++ api_request.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 
 // Replace with the access token you received (console err may pop up if token expires; go to api_fetch and run it for a new token)
-const accessToken = 'BQBjEPGuwexUlYEmlDXXyj8xquYXXDrFVcxuukD4rm2m2NoHm4J5NiVfhVWErIT8YKXBLgWRXWdHApGIfaTv-AhdssBnusHrZfG4DSbpS6njptzLreKt2d-otxtoLLoD0J8MjCjBpl0';  
+const accessToken = 'BQA64Pt6qHv2BUB3dWaQz_irkM3L2SwSemltkzF1ZwNRTznNKLDpV3vqLuPPv3iJ00hdYqQh_qoG1pOatT1RLdyoYiSykh5_SVHjcNsQm1vEmPg3P43XpmHuv2TePKee1aATb13dx3s';  
 
 // Define the query (e.g., a song name or artist)
-const query = "mess stray kids";
+const query = "wild babymonster";
 
 // Spotify's search endpoint for tracks
 const url = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track&limit=1`;
